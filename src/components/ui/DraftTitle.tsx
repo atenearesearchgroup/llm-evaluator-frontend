424850c01d5df16d0442a648efb22b77b32bac58
iLoopBreak <teogamer908@gmail.com>
2024-04-17 01:20:53 +0200

    feat: Added broadcast channel to synchronize DraftTitle with DraftGroupList

--- src/components/ui/DraftTitle.tsx
+++ src/components/ui/DraftTitle.tsx
@@ -25,14 +25,24 @@ const updateTitle = async (draftId: number, title: string) => {
 
 export const DraftTitle = ({ draftId, title }: DraftTitleProps) => {
     const [tempTitle, setTempTitle] = useState(title)
+    let channel = new BroadcastChannel('title_change')
 
     useEffect(() => {
         const timeout = setTimeout(() => {
-            updateTitle(draftId, tempTitle)
+            if (!updateTitle(draftId, tempTitle))
+                return
+
+            channel.postMessage({ newTitle: tempTitle, draftId })
         }, 1000)
 
         return () => clearTimeout(timeout)
     }, [tempTitle])
+
+    useEffect(() => {
+        channel = new BroadcastChannel('title_change')
+
+        return () => channel.close()
+    }, [])
 
     const handleTitleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         setTempTitle(e.target.value)
