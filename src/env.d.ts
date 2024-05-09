e4dcd53969f39cd14b9dac724b32c028548f49c1
iLoopBreak <teogamer908@gmail.com>
2024-05-09 13:49:48 +0200

    chore: Some style changes to pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly BACKEND_API_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
