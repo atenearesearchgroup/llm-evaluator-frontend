cac1b428103026d5c0c308bd91df33722a3228b0
iLoopBreak <teogamer908@gmail.com>
2024-06-10 21:08:06 +0200

    build: Removed Astro DB, as it won't be used

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
