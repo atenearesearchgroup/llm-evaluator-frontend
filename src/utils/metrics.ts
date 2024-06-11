201430eb651e6d87ebf37ae37b566b4069b2b68e
iLoopBreak <teogamer908@gmail.com>
2024-06-11 12:53:42 +0200

    chore: Changed the format of exported data from instances to make it easier for metrics

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -1,10 +1,18 @@
-import type { Chat, Message } from "@/model/chat";
+import type { Chat, Message, PromptIteration } from "@/model/chat";
 import type { IntentInstance } from "@/model/model";
 
 const dateFormat = new Intl.DateTimeFormat("es", {
     dateStyle: "short",
     timeStyle: "short"
 });
+
+type SerializedIteration = {
+    promptType: string,
+    prompts: number,
+    score: number,
+    messages: Message[],
+
+}
 
 export const exportJson = (instance: IntentInstance) => {
     const output = {
@@ -24,9 +32,22 @@ export const exportJson = (instance: IntentInstance) => {
     let lastDate = new Date(0)
 
     instance.chats.forEach((draft: Chat, idx) => {
-        if(!draft.finalized) return
-        
-        const messages : (Message & { promptType: string })[] = [] 
+        if (!draft.finalized) return
+
+        const messages: (Message & { promptType: string })[] = []
+
+        const iterations: (SerializedIteration)[] = []
+
+        draft.promptIterations.forEach((iteration) => {
+            const maxScore = iteration.messages.reduce((acc, message) => Math.max(acc, 'score' in message ? message.score : 0), 0)
+            iterations.push({
+                promptType: iteration.type, 
+                score: maxScore, 
+                prompts: iteration.messages.filter((message) => message.type === "user").length,
+                messages: iteration.messages,
+            })
+        })
+
 
         draft.promptIterations.forEach((iteration) => {
             iteration.messages.forEach((message) => {
@@ -37,10 +58,11 @@ export const exportJson = (instance: IntentInstance) => {
 
         const serializedDraft = {
             draftNumber: draft.draftNumber,
-            messages: messages
+            maxScore: iterations.reduce((acc, iteration) => acc + iteration.score, 0),
+            iterations: iterations
         }
 
-        lastDate = new Date(Math.max(lastDate.getTime(), ...serializedDraft.messages.map(message => Date.parse(message.timestamp as any as string))))
+        lastDate = new Date(Math.max(lastDate.getTime(), ...messages.map(message => Date.parse(message.timestamp as any as string))))
 
         output.chats.push(serializedDraft)
 
