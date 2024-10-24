fc810a70eef7758b1411db949cfd97b43b6a96b3
iLoopBreak <teogamer908@gmail.com>
2024-10-24 13:12:04 +0200

    chore: Changed exported metrics data structure

--- src/lib/metrics.ts
+++ src/lib/metrics.ts
@@ -1,5 +1,6 @@
 import type { DataInfo, InstanceInfo } from "@/hooks/useEvaluationData";
 import type { Chat, Message } from "@/model/chat";
+import type { CategoryError } from "@/model/evaluation";
 import type { EvaluationSettings, IntentInstance, ModelSettings } from "@/model/model";
 import { getInstance } from "@/services/instanceService";
 
@@ -8,17 +9,24 @@ const dateFormat = new Intl.DateTimeFormat("es", {
     timeStyle: "short"
 });
 
+type SerializedMessage = {
+    type: string,
+    text: string,
+    timestamp: string,
+    score?: number
+}
+
 type SerializedIteration = {
     promptType: string,
     prompts: number,
     score: number,
-    messages: Message[],
+    messages: SerializedMessage[],
 
 }
 
 export const exportJson = async (dataInfo: DataInfo) => {
-    const output = {
-        instances: [] as any[],
+    let output = {
+        instances: {} as Record<string, any>,
         modelSettings: undefined as ModelSettings | undefined,
         evaluationSettings: undefined as EvaluationSettings | undefined
     }
@@ -31,16 +39,15 @@ export const exportJson = async (dataInfo: DataInfo) => {
         }
 
         const json = generateJson(instance, instanceData)
-        output.instances.push(json)
+        output.instances[instance.intentModel?.displayName || instanceData.id] = json;
 
         if (!output.modelSettings) {
             const modelSettings = instance.modelSettings
 
             // remove id from modelSettings
-
             delete modelSettings.id
 
-            output.modelSettings = instance.modelSettings
+            output.modelSettings = modelSettings
         }
         if (!output.evaluationSettings)
             output.evaluationSettings = {
@@ -49,12 +56,18 @@ export const exportJson = async (dataInfo: DataInfo) => {
                 maxRepeatingPrompt: instance.maxRepeatingPrompt
             }
 
-        console.log("output", output)
     }
 
-    console.log("final output", output)
-
-    return output
+    const derivedOutput = {
+        options: {
+            model: output.modelSettings,
+            evaluation: output.evaluationSettings
+        },
+        instances: output.instances
+    }
+    console.log("final output", derivedOutput)
+    
+    return derivedOutput
 }
 
 const calculateMetrics = (instance: IntentInstance, instanceData: InstanceInfo) => {
@@ -63,48 +76,66 @@ const calculateMetrics = (instance: IntentInstance, instanceData: InstanceInfo) 
 
 const generateJson = (instance: IntentInstance, instanceData: InstanceInfo) => {
     const output = {
-        id: instance.id,
-        score: instanceData.evaluation?.score,
-        maxScore: instanceData.evaluation?.maxScore,
-        modelName: instance.intentModel?.displayName,
-        chats: [] as any[],
-        lastDate: ""
+        //id: instance.id,
+        //modelName: instance.intentModel?.displayName,
+        lastDate: "",
+        score: {
+            highest: -1,
+            last: instanceData.evaluation?.score,
+            max: instanceData.evaluation?.maxScore,
+            reason: undefined as string | undefined,
+            errors: undefined as CategoryError[] | undefined,
+            syntaxErrors: undefined as string[] | undefined,
+        },
+        chats: [] as any[]
     }
 
     let lastDate = new Date(0)
+    let highestScore = -2;
 
     instance.chats.forEach((draft: Chat) => {
         if (!draft.finalized) return
 
-        const messages: (Message & { promptType: string })[] = []
+        // const messages: (SerializedMessage)[] = []
 
         const iterations: (SerializedIteration)[] = []
 
         draft.promptIterations.forEach((iteration) => {
-            const maxScore = iteration.messages.reduce((acc, message) => Math.max(acc, 'score' in message ? message.score : 0), 0)
+            const maxScore = iteration.messages.reduce((acc, message) => Math.max(acc, 'score' in message ? message.score : -2), -2)
             iterations.push({
                 promptType: iteration.type,
                 score: maxScore,
                 prompts: iteration.messages.filter((message) => message.type === "user").length,
-                messages: iteration.messages,
+                messages: iteration.messages.map((message) => {
+                    return {
+                        type: message.type,
+                        text: message.content,
+                        timestamp: dateFormat.format(new Date(Date.parse(message.timestamp as any as string))),
+                        score: message.type === "ai" ? message.score : undefined
+                    }
+                }),
             })
         })
 
 
-        draft.promptIterations.forEach((iteration) => {
-            iteration.messages.forEach((message) => {
-                const messageCopy = { ...message, promptType: iteration.type }
-                messages.push(messageCopy)
-            })
-        })
+        // draft.promptIterations.forEach((iteration) => {
+        //     iteration.messages.forEach((message) => {
+        //         // const messageCopy = { ...message, promptType: iteration.type }
+        //         messages.push(message)
+        //     })
+        // })
 
         const serializedDraft = {
-            draftNumber: draft.draftNumber,
-            maxScore: iterations.reduce((acc, iteration) => Math.max(acc, iteration.score), 0),
+            chatNumber: draft.draftNumber,
+            highestScore: iterations.reduce((acc, iteration) => Math.max(acc, iteration.score), 0),
             iterations: iterations
         }
 
-        lastDate = new Date(Math.max(lastDate.getTime(), ...messages.map(message => Date.parse(message.timestamp as any as string))))
+        lastDate = new Date(Math.max(lastDate.getTime(),
+            ...draft.promptIterations
+                .flatMap(it=>it.messages)
+                .map(message => Date.parse(message.timestamp as any as string))))
+        highestScore = Math.max(highestScore, serializedDraft.highestScore)
 
         output.chats.push(serializedDraft)
 
@@ -112,6 +143,37 @@ const generateJson = (instance: IntentInstance, instanceData: InstanceInfo) => {
 
     const formattedDate = dateFormat.format(lastDate)
     output.lastDate = formattedDate
+    output.score.highest = highestScore
+
+    if(output.score.last === -1) {
+        output.score.reason = "No valid syntax"
+    } else {
+        delete output.score["reason"]
+    }
+
+    if(output.score.highest !== output.score.max) {
+        output.score.errors = instanceData.evaluation?.errors
+    } 
+
+    if(instanceData.evaluation?.syntaxErrors) {
+        output.score.syntaxErrors = instanceData.evaluation?.syntaxErrors;
+    } else delete output.score["syntaxErrors"]
+
+    if(!output.score.errors) {   
+        delete output.score["errors"]
+    }
+
+
+    // output.status = output.
 
     return output
+}
+
+const serializeMessage = (message: Message) => {
+    return {
+        type: message.type,
+        text: message.content,
+        timestamp: message.timestamp,
+        score: 'score' in message ? message.score : 0
+    }
 }
