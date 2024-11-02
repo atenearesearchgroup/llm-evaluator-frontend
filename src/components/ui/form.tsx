a53bbdce6fff025f06960584603e87a1931325b7
iLoopBreak <teogamer908@gmail.com>
2024-11-02 10:54:20 +0100

    build: Added context menu and changed shadcn components directory

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -10,7 +10,7 @@ import {
 import type { ControllerProps, FieldPath, FieldValues } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
-import { Label } from "@/components/shadcdn/ui/label"
+import { Label } from "@/components/ui/label"
 
 const Form = FormProvider
 
--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -5,8 +5,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/shadcdn/ui/toast"
-import { useToast } from "@/components/shadcdn/ui/use-toast"
+} from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
