a23b3cbeb902b4db71091d09a100ba6b18c41b63
iLoopBreak <teogamer908@gmail.com>
2024-12-12 23:42:32 +0100

    feat: Differentiate invalid instances in the execution bar visually and added delete button for invalid instances

--- src/components/execution/skeletons/NotFoundExecution.tsx
+++ src/components/execution/skeletons/NotFoundExecution.tsx
@@ -1,13 +1,54 @@
-import { Card, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { useToast } from "@/components/ui/use-toast";
+import { useEvaluationData } from "@/hooks/useEvaluationData";
 
 export type NotFoundExecutionProps = {
     id: number;
 }
 
 export const NotFoundExecution = ({ id }: NotFoundExecutionProps) => {
+    const { getExecutionInstance, delEvaluation } = useEvaluationData()
+    const { toast } = useToast()
     return (
         <Card>
             <CardTitle className="p-10 text-center">Couldn't find instances related to execution with id #{id}</CardTitle>
+            <CardContent className="flex items-center flex-col gap-4">
+                <p className="text-center">Please try again later or...</p>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="destructive">Delete local data</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogTitle>Do you really want to delete all the local data?</DialogTitle>
+                        <p className="text-primary">This action cannot be undone.</p>
+                        <div className="flex justify-end gap-2">
+                            <Button variant="destructive" onClick={() => {
+                                const dataInfo = getExecutionInstance(id)
+
+                                if (!dataInfo) return;
+
+                                delEvaluation(id)
+
+                                toast(
+                                    {
+                                        variant: "default",
+                                        description: "Execution deleted",
+                                    }
+                                )
+
+                                setTimeout(() => {
+                                    window.location.href = "/"
+                                }, 2000)
+                            }}>Yes</Button>
+                            <DialogClose asChild>
+                                <Button variant="secondary">No</Button>
+                            </DialogClose>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </CardContent>
         </Card>
     )
 }
--- src/components/sidebar/ExecutionInstanceButton.tsx
+++ src/components/sidebar/ExecutionInstanceButton.tsx
@@ -1,7 +1,9 @@
 import { Badge } from "@/components/ui/badge"
 import { Tooltip, TooltipProvider } from "@/components/ui/tooltip"
 import type { DataInfo } from "@/hooks/useEvaluationData"
+import { getInstance } from "@/services/instanceService"
 import { TooltipArrow, TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip"
+import { useEffect, useState } from "react"
 
 
 type IntentInstanceProps = {
@@ -12,6 +14,26 @@ type IntentInstanceProps = {
 
 // /bg-slate-600
 export const ExecutionInstanceButton = ({ instance, id }: IntentInstanceProps) => {
+    const [isValid, setValid] = useState<boolean>(true)
+    
+    useEffect(()=> {
+        let task = setTimeout(async () => {
+            if(instance.instances.length === 0) {
+                setValid(false)
+                return
+            }
+
+            const loadedInstance = await getInstance (instance.instances[0].id)
+            if(loadedInstance === null || `requestError` in loadedInstance) {
+                setValid(false)
+                return
+            }
+        })
+
+        return () => {
+            clearTimeout(task)
+        }
+    }, [])
 
     const isRunning = instance.instances.every(info => info.status === `running`)
      ? `in progress` : `completed`
@@ -20,19 +42,18 @@ export const ExecutionInstanceButton = ({ instance, id }: IntentInstanceProps) =
         <li
             id={`list-${id}`}
             data-id={`${id}`}
-            className=" draft-cell mx-2 sm:my-2 flex sm:flex-col
+            className={`draft-cell mx-2 sm:my-2 flex sm:flex-col
               bg-primary-foreground text-primary
-
-            bg-opacity-30 pl-5 p-3 sm:pl-3 sm:text-sm rounded-lg sm:justify-between gap-4 sm:gap-2"
+              bg-opacity-30 pl-5 p-3 sm:pl-3 sm:text-sm rounded-lg sm:justify-between gap-4 sm:gap-2 ` + (isValid ? "" : "bg-red-500 opacity-60")}
         >
-            <a href={`/execution/${id}`} className="font-bold draft-cell">
+            <a href={`/execution/${id}`} className="font-bold draft-cell text-wrap max-w-40" >
                 {instance.title ?? "Untitled"}
             </a>
             <TooltipProvider>
                 <Tooltip delayDuration={400}>
                     <TooltipTrigger  className="place-self-end">
                         <Badge variant="default" data-id={id} className="text-primary-foreground w-fit sm:text-xs place-self-end font-normal">
-                            {isRunning}
+                            {isValid ? isRunning : "Invalid instance"}
                         </Badge>
                     </TooltipTrigger>
                     <TooltipContent side={"right"} className="">
